a5a7db0d8e8f1830d56daba73c23c951a6152722
jessicamartins919 <jessicamartinsdesouza919@gmail.com>
2026-02-24 10:04:14 -0300

    Edição local do arquivo minha-funcionalidade.py

--- minha-funcionalidade.py
+++ minha-funcionalidade.py
@@ -1,2 +1,3 @@
 print("Nova funcionalidade implementada por Jéssica Martins")
 print("Aula prática - Gerenciamento de Configuração - 24/02/2026")
+print("=== EDITADO LOCALMENTE POR JÉSSICA EM 24/02/2026 ===")
